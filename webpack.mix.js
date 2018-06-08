e94983f63adaea7ff2d9da02db06a00ec8ef824f
Thabo Madibane <madibane.tt@gmail.com>
2018-06-08 19:27:31 +0200

    Bootsrap added to public directory, feather-icons added to gitignore

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,8 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
-   .copyDirectory('node_modules/feather-icons', 'public/feather-icons');
+   .copyDirectory('node_modules/feather-icons', 'public/feather-icons')
+   .copyDirectory('node_modules/bootstrap', 'public/bootstrap');
 
    if (mix.inProduction()) {
       mix.version();
